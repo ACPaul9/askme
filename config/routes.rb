c2bfff0224a4343afb281015cabb3573b60085a1
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-05-19 22:21:29 +0300

    Add registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   resources :questions do
     put 'hide', on: :member
   end
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create]
 
   root to: 'questions#index'
 end
