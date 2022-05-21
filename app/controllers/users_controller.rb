0fb49a3cd9df0f7ba37b7efb2f3af7f457976c10
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-05-21 16:08:14 +0300

    change user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def new
-    session[:current_time] = Time.now
     @user = User.new
   end
 
@@ -12,5 +11,5 @@ class UsersController < ApplicationController
     session[:user_id] = user.id
 
     redirect_to root_path, notice: 'Вы успешно зарегистрировались'
-    end
+  end
 end
