0ea7b23884bcf2599a1934b7ffb9e7ca58bed0d4
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-05-21 20:52:01 +0300

    Add validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
+
+  validates :body, presence: true, length: {maximum: 280}
+
   def hidden?
     hidden
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  before_save :downcase_nickname
+
+  validates :nickname, uniqueness: true, length: {maximum: 40}, format: { with: /\A[a-z_]+\d+\z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
