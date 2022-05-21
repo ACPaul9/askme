897c3cfa4443aa02c9bbc0a38aa5b51ec0fd72b2
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-05-22 00:06:16 +0300

    Add user color header

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
 
   before_save :downcase_nickname
 
-  validates :nickname, uniqueness: true, length: {maximum: 40}, format: { with: /\A[a-z_]+\d+\z/ }
+  validates :nickname, uniqueness: true, length: {maximum: 40}, format: { with: /\A[a-z_0-9]+\z/ }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
-
-
+  validates :header_color, format: {with: /\A#[a-f0-9]{6}\z/i }
+  
   def downcase_nickname
     nickname.downcase!
   end
