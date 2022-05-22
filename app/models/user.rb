14cd5f7f2b85a6aa2d4d6f89c01eb1fac71bebaa
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-05-22 19:58:45 +0300

    Fix errors in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
 
+  REGEXP_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  REGEXP_NIKNAME = /\A\w+\z/
+  REGEXP_COLOR = /\A#([[:xdigit:]]{3}){1,2}\z/
+
   before_save :downcase_nickname
 
-  validates :nickname, uniqueness: true, length: {maximum: 40}, format: { with: /\A[a-z_0-9]+\z/ }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
-  validates :header_color, format: {with: /\A#[a-f0-9]{6}\z/i }
-  
+  validates :nickname, uniqueness: true, length: {maximum: 40}, format: { with: REGEXP_NIKNAME }
+  validates :email, presence: true, uniqueness: true, format: { with: REGEXP_EMAIL }
+  validates :header_color, format: {with: REGEXP_COLOR }, on: :update
+
   def downcase_nickname
     nickname.downcase!
   end
